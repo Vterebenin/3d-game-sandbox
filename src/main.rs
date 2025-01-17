6e53a66f66b1d1423d9a9618cd6e55942acd2d44
Валентин Теребенин <v.terebenin@andersenlab.com>
2025-01-18 00:28:14 +0300

    feat: add a physics engine

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,16 @@
+use avian3d::prelude::*;
 use bevy::prelude::*;
 use blenvy::*;
 
-fn main() {
+fn main() -> AppExit {
     App::new()
-        .add_plugins((DefaultPlugins, BlenvyPlugin::default()))
-        // We need to register components to make them visible to Blenvy
-        .register_type::<Player>()
+        .add_plugins((
+            DefaultPlugins,
+            BlenvyPlugin::default(),
+            PhysicsPlugins::default(),
+        ))
         .add_systems(Startup, setup)
-        .run();
-}
-
-#[derive(Component, Reflect)]
-#[reflect(Component)]
-struct Player {
-    strength: f32,
-    perception: f32,
-    endurance: f32,
-    charisma: f32,
-    intelligence: f32,
-    agility: f32,
-    luck: f32,
+        .run()
 }
 
 fn setup(mut commands: Commands) {
@@ -30,4 +21,3 @@ fn setup(mut commands: Commands) {
         GameWorldTag,
     ));
 }
-
